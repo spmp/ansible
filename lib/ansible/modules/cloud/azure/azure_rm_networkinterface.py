0c09d8f640a8a6b69b5464e92a9386777ae3758b
Jasper Mackenzie <jmackenzie@wynyardgroup.com>
2018-07-27 12:03:25 +1200

    Add type to enable_ip_forwarding documentation

--- lib/ansible/modules/cloud/azure/azure_rm_networkinterface.py
+++ lib/ansible/modules/cloud/azure/azure_rm_networkinterface.py
@@ -184,8 +184,9 @@ options:
             - Whether to enable IP forwarding
         aliases:
             - ip_forwarding
-        version_added: 2.7
+        type: bool
         default: False
+        version_added: 2.7
 extends_documentation_fragment:
     - azure
     - azure_tags
